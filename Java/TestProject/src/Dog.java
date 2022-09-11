4accbcffb66c1d58a3c79284013a2fd89d91bce1
Yuval <yuval.karp.yu@gmail.com>
2022-09-11 11:03:54 +0300

    added some targilim(very easy)

---
+++ Java/TestProject/src/Dog.java
@@ -0,0 +1,11 @@
+public class Dog {
+    private int weight;
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public void setWeight(int newWeight){
+        this.weight = newWeight;
+    }
+}
---
+++ Java/TestProject/src/TestDog.java
@@ -0,0 +1,7 @@
+public class TestDog {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.setWeight(50);
+        System.out.println(d1.getWeight());
+    }
+}
